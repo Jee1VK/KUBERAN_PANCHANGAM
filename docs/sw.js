76148770989b8ec81612ddc6d444c4238a519696
Jee1VK <67321304+Jee1VK@users.noreply.github.com>
2026-09-20 21:09:06 +0530

    Add PWA with standalone JS engine for GitHub Pages (docs/)

---
+++ docs/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'kuberan-panchangam-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/images/kuberan_logo_white_bg.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
